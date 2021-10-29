c5b4788b8a40d3d230a2bac426a37b7628c78e90
Jonatan Alvarez <alvarez.geek@hotmail.com>
2021-10-28 19:04:16 -0500

    avance de app de noticias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'noticias';
+
+  buscarNoticias(parametros: any){
+    console.log('soy el padre');
+    console.log(parametros);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { ListadoNoticiasComponent } from './components/listado-noticias/listado-noticias.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    ListadoNoticiasComponent,
+    NavbarComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
